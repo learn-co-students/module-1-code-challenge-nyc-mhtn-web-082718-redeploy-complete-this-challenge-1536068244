d6897178eb68a532859ba72c2f63176a3f36e547
github username <github email address>
2018-09-04 10:42:59 -0400

    AAAAAHH

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,28 @@
+require "pry"
+
 class Restaurant
   attr_accessor :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    @@all.find do |restaurant|
+      restaurant.name == name
+    end
+  end
+
+  def customers
+    # got to iterate over an array that includes all the customers for that specific restaurant instance. This likely means a helper method before this that creates that array and using uniq on that
+  end
+
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,9 +3,23 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+
+c1 = Customer.new("Olivier", "Willems")
+c2 = Customer.new("Charlie", "Russo")
+c3 = Customer.new("May", "Kim")
+c4 = Customer.new("Jordan", "Farkas")
+c5 = Customer.new("Olivier", "Giroud")
+
+r1 = Restaurant.new("Upstate")
+r2 = Restaurant.new("The Smith")
+r3 = Restaurant.new("Han Dynasty")
+r4 = Restaurant.new("Blue Hill")
+
+rev1 = Review.new(c1, r2, 3)
+rev2 = Review.new(c2, r1, 4)
+rev3 = Review.new(c1, r3, 4)
+rev4 = Review.new(c3, r4, 5)
+rev5 = Review.new(c5, r4, 5)
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
