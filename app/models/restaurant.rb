94f58d5f163fc0d946f3404074e8b7a33b27de2e
yapjinai <jinai.jyap@gmail.com>
2018-09-04 10:34:44 -0400

    Finished Mod 1 code challenge

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,32 @@
 class Restaurant
-  attr_accessor :name
+  @@all = []
+  attr_accessor :name, :reviews
 
   def initialize(name)
     @name = name
+    @reviews = []
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    @@all.find { |rs| rs.name == name }
+  end
+
+  def customers
+    @reviews.map { |rv| rv.customer }.uniq
+  end
+
+  def average_star_rating
+    @reviews.map { |rv| rv.rating.to_f }.inject(:+) / @reviews.count.to_f
+  end
+
+  def longest_review
+    @reviews.sort_by { |rv| rv.content.length }.reverse[0]
   end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,22 @@
 class Review
-  
-end
+  @@all = []
+  attr_accessor :customer, :restaurant, :content, :rating
 
+  def initialize(customer, restaurant, content, rating)
+    @customer = customer
+    @restaurant = restaurant
+    @content = content
+    @rating = rating
+
+    @@all << self
+
+    restaurant.reviews << self
+
+    customer.reviews << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
