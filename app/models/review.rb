be5046cbb524e015d7999a5bf448c6b4c3448cb4
sinyoungnam <sinnam90@gmail.com>
2018-09-04 10:41:37 -0400

    done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,43 @@
+#review belongs to restaurant and customer
 class Review
-  
+
+  attr_accessor :title, :rating, :content, :customer,
+  attr_reader :author, :restaurant
+
+  @@all = []
+
+  def initialize(title, author, rating, content, restaurant, customer)
+    @title = title
+    @author = author
+    @rating = rating
+    @content = content
+    @restaurant = restaurant
+    @customer = customer
+    @@all << self
+  end
+
+# - `Review.all` - returns all of the reviews
+  def self.all
+    @@all
+  end
+
 end
 
+
+
+# - `Review#customer`
+#   - returns the customer object for that given review
+#   - Once a review is created, I should not be able to change the author DONE
+
+
+# - `Review#restaurant`
+#   - returns the restaurant object for that given review
+#   - Once a review is created, I should not be able to change the restaurant DONE
+
+
+# - `Review#rating`
+#   - returns the star rating for a restaurant. This should be an integer from 1-5
+
+
+# - `Review#content`
+#   - returns the review content, as a string, for a particular review
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,18 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+r1 = Restaurant.new("papa johns")
+r2 = Restaurant.new("pizza hut")
+r3 = Restaurant.new("dominos")
+
+c1 = Customer.new("stacy", "dice")
+c2 = Customer.new("patrick", "mud")
+c3 = Customer.new("mike", "kale")
+
+#title, author, rating, restaurant, customer
+review1 = Review.new("good-good", "luigi", 5, "this is good", r1, c1)
+review2 = Review.new("yummy", "peach", 3, "this is bad", r2, c2)
+review3 = Review.new("so bad", "mario", 1, "superb", r3, c3)
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
