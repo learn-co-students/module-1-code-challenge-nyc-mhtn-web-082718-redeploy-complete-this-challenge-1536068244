7433812e506f00cd2866db4daf115a3e7cc84265
github username <github email address>
2018-09-04 10:42:09 -0400

    My life is in your hands now

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,25 @@
 class Restaurant
+  #many to many relationship with customer joined through reviews
+  #many to many with reviews
+
   attr_accessor :name
+
+  @@all = []
 
   def initialize(name)
     @name = name
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    Restaurant.all.find do |restaurant|
+      restaurant.name == name
+    end
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,20 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+c1 = Customer.new("Chris", "Mierzwa")
+c2 = Customer.new("Ty", "Simcox")
+c3 = Customer.new("Eric", "Holenberg")
+c4 = Customer.new("Michael", "Schwartz")
+
+r1 = Restaurant.new("Accents")
+r2 = Restaurant.new("Chilis")
+r3 = Restaurant.new("BWW")
+r4 = Restaurant.new("Fridays")
+
+rev1 = Review.new(1, "Worst place", c1, r1)
+rev2 = Review.new(2, "Would eat there again", c2, r2)
+rev3 = Review.new(5, "favorite place", c3, r3)
+rev4 = Review.new(3, "so so", c4, r4)
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
