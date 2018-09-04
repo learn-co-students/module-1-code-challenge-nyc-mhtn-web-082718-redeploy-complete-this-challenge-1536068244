44393058cd3f18414900fbe54488d25000cb1636
miriamfrank23 <miriamfrank23@gmail.com>
2018-09-04 10:41:43 -0400

    Miriam Mod 1 Assessment

--- tools/console.rb
+++ tools/console.rb
@@ -3,6 +3,16 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
+
+
+c1 = Customer.new("Uncle", "Sam")
+c2 = Customer.new("Matt", "Damon")
+r1 = Restaurant.new("Wendy's")
+r2 = Restaurant.new("Taco Bell")
+rv1 = Review.new(c1, r1, 5, "hi")
+rv2 = Review.new(c2, r2, 1, "bye")
+rv3 = Review.new(c2, r1, 1, "bye")
+
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
