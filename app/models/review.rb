c1c90bb085b743523525f996e7cd172d655feb96
Chris Bojemski <chris.bojemski@gmail.com>
2018-09-04 10:36:57 -0400

    Everything's working (#money 💸)

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,19 @@
 class Review
-  
-end
+  @@all = []
 
+  def self.all
+    @@all
+  end
+
+  attr_reader :author, :restaurant
+  attr_accessor :rating, :content
+
+  def initialize(author, restaurant, rating, content)
+    @author = author
+    @restaurant = restaurant
+    @rating = rating
+    @content = content
+
+    @@all << self
+  end
+end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,34 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+c1 = Customer.new('Chris', 'Bojemski')
+c2 = Customer.new('Evans', 'Wang')
+c3 = Customer.new('Jordan', 'Farkas')
+c4 = Customer.new('Jordan', 'Sternberg')
+
+r1 = Restaurant.new("BK")
+r2 = Restaurant.new("Red Lobster")
+r3 = Restaurant.new("McDonald's")
+
+# Testing making a Review obj...
+re1 = Review.new(c1, r1, 4, "I liked it.")
+re2 = Review.new(c2, r2, 3, "I have no opinion.")
+
+# Testing adding reviews...
+re3 = c1.add_review(r3, "It's the same as the other one.", 3)
+re4 = c1.add_review(r3, "Another review for the same McDs.", 3)
+
+# Testing getting multiple reviews for a single restaurant...
+re5 = c2.add_review(r1, "I destroy these fries like I destroy America's enemies. (With artillary.)", 5)
+re6 = c3.add_review(r1, "All I eat are almonds.", 2)
+re7 = c3.add_review(r1, "No, seriously that's it.", 1)
+
+c1.num_reviews
+c1.restaurants
+
+r1.reviews
+r1.average_star_rating
+r1.longest_review
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
