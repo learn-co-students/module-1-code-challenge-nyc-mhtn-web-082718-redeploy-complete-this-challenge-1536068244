00119cd6baf69ac6eb467addd7015001119f8651
danmj <daniel.jung@stern.nyu.edu>
2018-09-04 10:41:11 -0400

    module 1 code challenge

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,47 @@
+require 'pry'
+
 class Customer
   attr_accessor :first_name, :last_name
+
+  @@all = []
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    if self == name
+      return name
+    end
+  end
+
+  def self.find_all_by_first_name(name)
+    @@all.select {|string| string == name}
+  end
+
+  def self.all_names
+    @@all.collect {|x| x.full_name}
+  end
+
+  def add_review(restaurant, content, rating)
+    newreview = Review.new(restaurant, content, rating)
+  end
+
+  def num_reviews
+    Customer.count
+  end
+
+  def restaurants
+    
   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,28 @@
-class Review
-  
-end
+require 'pry'
 
+class Review
+  attr_reader :name
+  @@all = []
+
+  def initialize(name)x
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def customer(self)
+    self.Customer
+  end
+
+  def restaurant(self)
+    self.Restaurant
+  end
+
+  def rating
+  end
+
+  def content
+  end
+end
