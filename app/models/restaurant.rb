5354cc9458a35cf0fdf54f047b16ea893564a535
github username <github email address>
2018-09-04 10:30:31 -0400

    module 1 test complete

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,55 @@
 class Restaurant
   attr_accessor :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    @@all.find {|restaurant| restaurant.name == name}
+  end
+
+  def customers
+    array = []
+    Review.all.each do |single_review|
+      array << single_review.customer if single_review.restaurant == self
+    end
+    array.uniq
+  end
+
+  def reviews
+    Review.all.select do |single_review|
+      single_review.restaurant == self
+    end
+  end
+
+  def average_star_rating
+    total = 0
+    own_reviews = self.reviews
+    own_reviews.each do |single_review|
+      total += single_review.rating
+    end
+    total.to_f / own_reviews.length
+  end
+
+  def longest_review
+    current_content = ""
+    own_reviews = self.reviews
+
+    own_reviews.each do |single_review|
+      if current_content == ""
+        current_content = single_review.content
+      elsif current_content.length < single_review.content.length
+        current_content = single_review.content
+      end
+    end
+    current_content
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,4 +1,7 @@
 require_relative '../config/environment.rb'
+require_relative '../app/models/customer.rb'
+require_relative '../app/models/restaurant.rb'
+require_relative '../app/models/review.rb'
 
 def reload
   load 'config/environment.rb'
@@ -6,6 +9,22 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+
+customer1 = Customer.new("Peter", "Griffin")
+customer2 = Customer.new("Glenn", "Quagmire")
+customer3 = Customer.new("Joe", "Swanson")
+customer4 = Customer.new("Peter", "Parker")
+restaurant1 = Restaurant.new("Hell's Kitchen")
+restaurant2 = Restaurant.new("Pizza Hut")
+restaurant3 = Restaurant.new("Dominos")
+restaurant4 = Restaurant.new("Papa Johns")
+review1 = Review.new(restaurant1, customer1, "I like to watch people get yelled at.", 5)
+review2 = Review.new(restaurant2, customer2, "This isn't a hut at all!", 1)
+review3 = Review.new(restaurant3, customer3, "This is extremely alright.", 3)
+review4 = Review.new(restaurant4, customer4, "My papa's name aint John!", 1)
+review5 = Review.new(restaurant1, customer2, "Giggity", 4)
+review6 = Review.new(restaurant1, customer2, "Actually nah its not that great", 3)
+review7 = Review.new(restaurant4, customer1, "Hey this restaurant must belong to my dad!", 3)
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
