444754e88533b1c69f1c69504fd1a2bc48cdbd12
github username <github email address>
2018-09-04 10:42:05 -0400

    done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,76 @@
 class Customer
   attr_accessor :first_name, :last_name
+  @@all = []
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    @@all.find do |cust|
+      name == cust.full_name
+    end
+  end
+
+  def self.find_all_by_first_name(name)
+    # names = []
+    @@all.select do |cust|
+      name == cust.first_name
+    end
+  end
+
+  def self.all_names
+    @@all.map do |cust|
+      cust.full_name
+    end
+  end
+
+  def add_review(restaurant, content, rating)
+    Review.new(self, restaurant, content, rating)
+  end
+
+  def num_reviews
+    total = 0
+    Review.all.each do |rev|
+      if rev.customer == self
+        total += 1
+      end
+    end
+    total
+  end
+
+  def restaurants
+    rest = []
+    Review.all.each do |rev|
+      if rev.customer == self
+        rest << rev.restaurant
+      end
+    end
+    rest.uniq
+  end
+
+
 end
+
+
+# Customer.all done
+# should return all of the customer instances
+
+# Customer.find_by_name(name) done
+# given a string of a full name, returns the first customer whose full name matches
+
+# Customer.find_all_by_first_name(name) Done
+# given a string of a first name, returns an array containing all customers with that first name
+
+# Customer.all_names Done
+# should return an array of all of the customer full names
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,74 @@
 class Restaurant
   attr_accessor :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    @@all.find do |rest|
+      rest.name == name
+    end
+  end
+
+  def customers
+    cust = []
+    Review.all.each do |rev|
+      if rev.restaurant == self
+        cust << rev.customer
+      end
+    end
+    cust.uniq
+  end
+
+  def reviews
+    Review.all.select do |rev|
+      rev.restaurant == self
+    end
+  end
+
+  def average_star_rating
+    sum = 0
+    count = 0
+    Review.all.each do |rev|
+      if rev.restaurant == self
+        sum += rev.rating
+        count += 1
+      end
+    end
+    sum / count
+  end
+
+  def longest_review
+    h = {}
+    Review.all.each do |rev|
+      if rev.restaurant == self
+        h[rev] = rev.content.length
+      end
+    end
+    h.max_by { |rev, length| length }
+  end
+
+  # def longest_review
+  #   reviews << self.reviews
+  #   reviews.each do |rev|
+  #     rev.sort_by {|rev.content| word.length}
+  #   end
+  # end dosnt work yet
+
+
 end
+
+
+
+# Restaurant.all Done
+# returns an array of all restaurants
+#
+# Restaurant.find_by_name(name) Done
+# given a string of restaurant name, returns the first restaurant that matches
