885de3c28480a3af1749333e3a837ff923b78023
JStern95 <JSternberg95@gmail.com>
2018-09-04 10:42:12 -0400

    Delicious Dumplings

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,42 @@
 class Restaurant
   attr_accessor :name
 
+  @@all =[]
+
   def initialize(name)
     @name = name
+    @@all.push(self)
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|info| info.name == name}
+  end
+
+  def customers
+    #Select when reviews.all.restaurant == self.name
+    #Push all customers names into an array
+    #Use uniq to filter out duplicates
+  end
+
+  def reviews
+    #Select when reviews.all.name == self.name
+  end
+
+  def average_star_rating
+    #Use Review method
+    #Add all of the review values as floats
+    #Divide by length of review array
+  end
+
+  def longest_review
+    #Use Review method
+    #Sort_by length and reverse and then .first
+  end
+
   end
 
 end
